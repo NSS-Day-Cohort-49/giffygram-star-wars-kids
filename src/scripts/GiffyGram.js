6ad2b08af4decbd966574544f54ee56953918bd5
AlexLam12 <alex.e015@gmail.com>
2021-06-29 10:35:09 -0500

    Posts now displaying

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,5 +1,10 @@
+import { listPost } from "./feed/PostList.js"
+
 export const GiffyGram = () => {
 
     // Show main main UI
-    return `<h1>Giffygram</h1>`
+    return `<h1>Giffygram</h1>
+        <section class="post__list">
+            ${listPost()}
+        </section>`
 }
