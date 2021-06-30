099d99c154f80fb30c98321eea05c642d86b39ac
ambarnett <andy.barnett09@gmail.com>
2021-06-30 12:00:39 -0500

    messages now show who they are from

--- src/scripts/friends/DirectMessage.js
+++ src/scripts/friends/DirectMessage.js
@@ -1,6 +1,6 @@
 //This will show all the messages that the user has recieved
 import { NavBar } from "../nav/NavBar.js"
-import { fetchMessages, getMessages } from "../data/provider.js"
+import { fetchMessages, getMessages, getUsers } from "../data/provider.js"
 const applicationElement = document.querySelector(".giffygram")
 
 export const ShowMessages = () => {
@@ -15,17 +15,25 @@ export const ShowMessages = () => {
 }
 export const DirectMessages = () => {
     const messages = getMessages()
+    const users = getUsers()
+    let userPost = ""
 
     let html = `
     <ul>
-        ${messages.map(
-            (message) => `
+        ${messages.map((message) => {
+            for(const user of users){
+                if(user.id === message.userId) {
+                    userPost = user
+                }
+            } 
+            
+            return `
             <section>
-                <h2 class="">From ${message.userId}</h2>
+                <h2 class="">From ${userPost.name}</h2>
                 <div class+"">${message.text}</div>
 
             `
-        ).join("")
+        }).join("")
         }
     `
     return html
